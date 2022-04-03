47c852f4e5ceafe3b70c4f9f7c1db90673b468a6
elenavanilla <81619723+elenavanilla@users.noreply.github.com>
2022-04-03 22:18:46 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.ctert5snvebo.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "wongchithiam-employee"
+customregion = "us-east-1"
